02fc8847649bf4bccb1eb141b036c9621fccf770
dangnhon <dang.nhon12345@yahoo.com>
2021-02-26 23:32:42 -0800

    create actions

--- backend_app_track/app/controllers/jobs_controller.rb
+++ backend_app_track/app/controllers/jobs_controller.rb
@@ -1,7 +1,18 @@
 class JobsController < ApplicationController
-
+    
     def index
         jobs = Job.all 
         render json: jobs  
+    end 
+
+    def create 
+        job = Job.create(job_params)
+        render json: job 
+    end 
+
+    private 
+    
+    def job_params
+        params.permit(:company_name, :job_title, :date, :salary, :status, :note, :contact_number, :user_id)
     end 
 end
--- backend_app_track/app/controllers/meetups_controller.rb
+++ backend_app_track/app/controllers/meetups_controller.rb
@@ -1,2 +1,13 @@
 class MeetupsController < ApplicationController
+
+    def create 
+        meetup = Meetup.create(meetup_params)
+        render json: meetup
+    end 
+
+    private 
+    
+    def meetup_params
+        params.permit(:name, :location, :date, :date, :user_id)
+    end
 end
